8e3ac5df997fcda8d28fa1adc385f6a90aa72105
darrenwang18 <darrenwang2000@gmail.com>
2024-12-08 15:40:48 -0500

    imports

--- src/Components/gi/data/images.js
+++ src/Components/gi/data/images.js
@@ -1,12 +1,191 @@
 // Character imports
+import Albedo from '../../../assets/gi/char/Albedo.webp';
+import Alhaitham from '../../../assets/gi/char/Alhaitham.webp';
+import Aloy from '../../../assets/gi/char/Aloy.webp';
+import Amber from '../../../assets/gi/char/Amber.webp';
+import AratakiItto from '../../../assets/gi/char/AratakiItto.webp';
+import Arlecchino from '../../../assets/gi/char/Arlecchino.webp';
+import Baizhu from '../../../assets/gi/char/Baizhu.webp';
+import Barbara from '../../../assets/gi/char/Barbara.webp';
+import Beidou from '../../../assets/gi/char/Beidou.webp';
+import Bennett from '../../../assets/gi/char/Bennett.webp';
+import Candace from '../../../assets/gi/char/Candace.webp';
+import Charlotte from '../../../assets/gi/char/Charlotte.webp';
+import Chasca from '../../../assets/gi/char/Chasca.webp';
+import Chevreuse from '../../../assets/gi/char/Chevreuse.webp';
+import Chiori from '../../../assets/gi/char/Chiori.webp';
+import Chongyun from '../../../assets/gi/char/Chongyun.webp';
+import Clorinde from '../../../assets/gi/char/Clorinde.webp';
+import Collei from '../../../assets/gi/char/Collei.webp';
+import Cyno from '../../../assets/gi/char/Cyno.webp';
+import Dehya from '../../../assets/gi/char/Dehya.webp';
+import Diluc from '../../../assets/gi/char/Diluc.webp';
+import Diona from '../../../assets/gi/char/Diona.webp';
+import Dori from '../../../assets/gi/char/Dori.webp';
+import Emilie from '../../../assets/gi/char/Emilie.webp';
+import Eula from '../../../assets/gi/char/Eula.webp';
+import Faruzan from '../../../assets/gi/char/Faruzan.webp';
+import Fischl from '../../../assets/gi/char/Fischl.webp';
+import Freminet from '../../../assets/gi/char/Freminet.webp';
+import Furina from '../../../assets/gi/char/Furina.webp';
+import Gaming from '../../../assets/gi/char/Gaming.webp';
+import Ganyu from '../../../assets/gi/char/Ganyu.webp';
+import Gorou from '../../../assets/gi/char/Gorou.webp';
+import HuTao from '../../../assets/gi/char/HuTao.webp';
+import Jean from '../../../assets/gi/char/Jean.webp';
+import Kachina from '../../../assets/gi/char/Kachina.webp';
+import KaedeharaKazuha from '../../../assets/gi/char/KaedeharaKazuha.webp';
+import Kaeya from '../../../assets/gi/char/Kaeya.webp';
+import KamisatoAyaka from '../../../assets/gi/char/KamisatoAyaka.webp';
+import KamisatoAyato from '../../../assets/gi/char/KamisatoAyato.webp';
+import Kaveh from '../../../assets/gi/char/Kaveh.webp';
+import Keqing from '../../../assets/gi/char/Keqing.webp';
+import Kinich from '../../../assets/gi/char/Kinich.webp';
+import Kirara from '../../../assets/gi/char/Kirara.webp';
+import Klee from '../../../assets/gi/char/Klee.webp';
+import KujouSara from '../../../assets/gi/char/KujouSara.webp';
+import KukiShinobu from '../../../assets/gi/char/KukiShinobu.webp';
+import Layla from '../../../assets/gi/char/Layla.webp';
+import Lisa from '../../../assets/gi/char/Lisa.webp';
+import Lynette from '../../../assets/gi/char/Lynette.webp';
+import Lyney from '../../../assets/gi/char/Lyney.webp';
+import Mika from '../../../assets/gi/char/Mika.webp';
+import Mona from '../../../assets/gi/char/Mona.webp';
+import Mualani from '../../../assets/gi/char/Mualani.webp';
+import Nahida from '../../../assets/gi/char/Nahida.webp';
+import Navia from '../../../assets/gi/char/Navia.webp';
+import Neuvillette from '../../../assets/gi/char/Neuvillette.webp';
+import Nilou from '../../../assets/gi/char/Nilou.webp';
+import Ningguang from '../../../assets/gi/char/Ningguang.webp';
+import Noelle from '../../../assets/gi/char/Noelle.webp';
+import Ororon from '../../../assets/gi/char/Ororon.webp';
+import Qiqi from '../../../assets/gi/char/Qiqi.webp';
 import RaidenShogun from '../../../assets/gi/char/RaidenShogun.webp';
+import Razor from '../../../assets/gi/char/Razor.webp';
+import Rosaria from '../../../assets/gi/char/Rosaria.webp';
+import SangonomiyaKokomi from '../../../assets/gi/char/SangonomiyaKokomi.webp';
+import Sayu from '../../../assets/gi/char/Sayu.webp';
+import Sethos from '../../../assets/gi/char/Sethos.webp';
+import Shenhe from '../../../assets/gi/char/Shenhe.webp';
+import ShikanoinHeizou from '../../../assets/gi/char/ShikanoinHeizou.webp';
+import Sigewinne from '../../../assets/gi/char/Sigewinne.webp';
+import Sucrose from '../../../assets/gi/char/Sucrose.webp';
+import Tartaglia from '../../../assets/gi/char/Tartaglia.webp';
+import Thoma from '../../../assets/gi/char/Thoma.webp';
+import Tighnari from '../../../assets/gi/char/Tighnari.webp';
+import Venti from '../../../assets/gi/char/Venti.webp';
+import Wanderer from '../../../assets/gi/char/Wanderer.webp';
+import Wriothesley from '../../../assets/gi/char/Wriothesley.webp';
+import Xiangling from '../../../assets/gi/char/Xiangling.webp';
+import Xianyun from '../../../assets/gi/char/Xianyun.webp';
+import Xiao from '../../../assets/gi/char/Xiao.webp';
+import Xilonen from '../../../assets/gi/char/Xilonen.webp';
+import Xingqiu from '../../../assets/gi/char/Xingqiu.webp';
+import Xinyan from '../../../assets/gi/char/Xinyan.webp';
+import YaeMiko from '../../../assets/gi/char/YaeMiko.webp';
+import Yanfei from '../../../assets/gi/char/Yanfei.webp';
+import Yaoyao from '../../../assets/gi/char/Yaoyao.webp';
+import Yelan from '../../../assets/gi/char/Yelan.webp';
+import Yoimiya from '../../../assets/gi/char/Yoimiya.webp';
+import YunJin from '../../../assets/gi/char/YunJin.webp';
+import Zhongli from '../../../assets/gi/char/Zhongli.webp';
 
 // Weapon imports
 import EngulfingLightning from '../../../assets/gi/weap/EngulfingLightning.webp';
 
 const characterImages = {
+  Albedo,
+  Alhaitham,
+  Aloy,
+  Amber,
+  AratakiItto,
+  Arlecchino,
+  Baizhu,
+  Barbara,
+  Beidou,
+  Bennett,
+  Candace,
+  Charlotte,
+  Chasca,
+  Chevreuse,
+  Chiori,
+  Chongyun,
+  Clorinde,
+  Collei,
+  Cyno,
+  Dehya,
+  Diluc,
+  Diona,
+  Dori,
+  Emilie,
+  Eula,
+  Faruzan,
+  Fischl,
+  Freminet,
+  Furina,
+  Gaming,
+  Ganyu,
+  Gorou,
+  HuTao,
+  Jean,
+  Kachina,
+  KaedeharaKazuha,
+  Kaeya,
+  KamisatoAyaka,
+  KamisatoAyato,
+  Kaveh,
+  Keqing,
+  Kinich,
+  Kirara,
+  Klee,
+  KujouSara,
+  KukiShinobu,
+  Layla,
+  Lisa,
+  Lynette,
+  Lyney,
+  Mika,
+  Mona,
+  Mualani,
+  Nahida,
+  Navia,
+  Neuvillette,
+  Nilou,
+  Ningguang,
+  Noelle,
+  Ororon,
+  Qiqi,
   RaidenShogun,
+  Razor,
+  Rosaria,
+  SangonomiyaKokomi,
+  Sayu,
+  Sethos,
+  Shenhe,
+  ShikanoinHeizou,
+  Sigewinne,
+  Sucrose,
+  Tartaglia,
+  Thoma,
+  Tighnari,
+  Venti,
+  Wanderer,
+  Wriothesley,
+  Xiangling,
+  Xianyun,
+  Xiao,
+  Xilonen,
+  Xingqiu,
+  Xinyan,
+  YaeMiko,
+  Yanfei,
+  Yaoyao,
+  Yelan,
+  Yoimiya,
+  YunJin,
+  Zhongli,
 };
+
 
 const weaponImages = {
   EngulfingLightning,
