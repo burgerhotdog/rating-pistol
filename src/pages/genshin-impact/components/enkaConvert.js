40346b260086396a258ba740a7d57f1cb7eebe3e
Darren Wang <darrenwang2000@gmail.com>
2025-02-14 16:56:44 -0500

    formatting

--- src/pages/genshin-impact/components/enkaConvert.js
+++ src/pages/genshin-impact/components/enkaConvert.js
@@ -1,159 +1,84 @@
-const enkaConvertChar = {  
-  // Version 5.3
+const enkaConvertChar = {
   10000107: `Citlali`,
   10000108: `Lan Yan`,
   10000106: `Mavuika`,
   "10000007-2": `Traveler (Pyro)`,
   "10000005-2": `Traveler (Pyro)`,
-  
-  // Version 5.2
   10000104: `Chasca`,
   10000105: `Ororon`,
-  
-  // Version 5.1
   10000103: `Xilonen`,
-  
-  // Version 5.0
   10000100: `Kachina`,
   10000101: `Kinich`,
   10000102: `Mualani`,
-  
-  // Version 4.8
   10000099: `Emilie`,
-  
-  // Version 4.7
   10000098: `Clorinde`,
   10000097: `Sethos`,
   10000095: `Sigewinne`,
-  
-  // Version 4.6
   10000096: `Arlecchino`,
-
-  // Version 4.5
   10000094: `Chiori`,
-  
-  // Version 4.4
   10000092: `Gaming`,
   10000093: `Xianyun`,
-  
-  // Version 4.3
   10000090: `Chevreuse`,
   10000091: `Navia`,
-  
-  // Version 4.2
   10000088: `Charlotte`,
   10000089: `Furina`,
-  
-  // Version 4.1
   10000087: `Neuvillette`,
   10000086: `Wriothesley`,
-  
-  // Version 4.0
   10000085: `Freminet`,
   10000083: `Lynette`,
   10000084: `Lyney`,
   "10000007-3": `Traveler (Hydro)`,
   "10000005-3": `Traveler (Hydro)`,
-  
-  // Version 3.8
-  
-  // Version 3.7
   10000061: `Kirara`,
-  
-  // Version 3.6
   10000082: `Baizhu`,
   10000081: `Kaveh`,
-  
-  // Version 3.5
   10000079: `Dehya`,
   10000080: `Mika`,
-  
-  // Version 3.4
   10000078: `Alhaitham`,
   10000077: `Yaoyao`,
-  
-  // Version 3.3
   10000076: `Faruzan`,
   10000075: `Wanderer`,
-  
-  // Version 3.2
   10000074: `Layla`,
   10000073: `Nahida`,
-  
-  // Version 3.1
   10000072: `Candace`,
   10000071: `Cyno`,
   10000070: `Nilou`,
-  
-  // Version 3.0
   10000067: `Collei`,
   10000068: `Dori`,
   10000069: `Tighnari`,
   "10000007-8": `Traveler (Dendro)`,
   "10000005-8": `Traveler (Dendro)`,
-  
-  // Version 2.8
   10000059: `Shikanoin Heizou`,
-  
-  // Version 2.7
   10000065: `Kuki Shinobu`,
   10000060: `Yelan`,
-  
-  // Version 2.6
   10000066: `Kamisato Ayato`,
-  
-  // Version 2.5
   10000058: `Yae Miko`,
-  
-  // Version 2.4
   10000063: `Shenhe`,
   10000064: `Yun Jin`,
-  
-  // Version 2.3
   10000057: `Arataki Itto`,
   10000055: `Gorou`,
-  
-  // Version 2.2
   10000050: `Thoma`,
-  
-  // Version 2.1
   10000062: `Aloy`,
   10000056: `Kujou Sara`,
   10000052: `Raiden Shogun`,
   10000054: `Sangonomiya Kokomi`,
-  
-  // Version 2.0
   10000002: `Kamisato Ayaka`,
   10000053: `Sayu`,
   "10000007-7": `Traveler (Electro)`,
   "10000005-7": `Traveler (Electro)`,
   10000049: `Yoimiya`,
-  
-  // Version 1.6
   10000047: `Kaedehara Kazuha`,
-  
-  // Version 1.5
   10000051: `Eula`,
   10000048: `Yanfei`,
-  
-  // Version 1.4
   10000045: `Rosaria`,
-  
-  // Version 1.3
   10000046: `Hu Tao`,
   10000026: `Xiao`,
-  
-  // Version 1.2
   10000038: `Albedo`,
   10000037: `Ganyu`,
-  
-  // Version 1.1
   10000039: `Diona`,
   10000033: `Tartaglia`,
   10000044: `Xinyan`,
   10000030: `Zhongli`,
-  
-  // Version 1.0
   10000021: `Amber`,
   10000014: `Barbara`,
   10000024: `Beidou`,
@@ -182,23 +107,16 @@ const enkaConvertChar = {
 };
 
 const enkaConvertWeap = {
-  // Version 5.3
   12514: `A Thousand Blazing Suns`,
   14517: `Starcaller's Watch`,
-  
-  // Version 5.2
   15514: `Astral Vulture's Crimson Plumage`,
   11432: `Calamity of Eshu`,
   15430: `Flower-Wreathed Feathers`,
   14430: `Waveriding Whirl`,
-  
-  // Version 5.1
   12430: `Fruitful Hook`,
   13430: `Mountain-Bracing Bolt`,
   11516: `Peak Patrol Song`,
   11430: `Sturdy Bone`,
-  
-  // Version 5.0
   14427: `Ash-Graven Drinking Horn`,
   15431: `Chain Breaker`,
   12431: `Earth Shaker`,
@@ -207,34 +125,18 @@ const enkaConvertWeap = {
   13431: `Footprint of the Rainbow`,
   14431: `Ring of Yaxche`,
   14516: `Surf's Up`,
-  
-  // Version 4.8
   13513: `Lumidouce Elegy`,
-  
-  // Version 4.7
   11515: `Absolution`,
   15426: `Cloudforged`,
   15513: `Silvershower Heartstrings`,
-  
-  // Version 4.6
   13512: `Crimson Moon's Semblance`,
-  
-  // Version 4.5
   13426: `Dialogues of the Desert Sages`,
   11514: `Uraku Misugiri`,
-  
-  // Version 4.4
   14515: `Crane's Echoing Call`,
-  
-  // Version 4.3
   12426: `"Ultimate Overlord's Mega Magic Sword"`,
   12512: `Verdict`,
-  
-  // Version 4.2
   11513: `Splendor of Tranquil Waters`,
   11428: `Sword of Narzissenkreuz`,
-  
-  // Version 4.1
   14426: `Ballad of the Boundless Blue`,
   14513: `Cashflow Supervision`,
   12427: `Portable Power Saw`,
@@ -242,8 +144,6 @@ const enkaConvertWeap = {
   15427: `Range Gauge`,
   11427: `The Dockhand's Assistant`,
   14514: `Tome of the Eternal Flow`,
-  
-  // Version 4.0
   13424: `Ballad of the Fjords`,
   11425: `Finale of the Deep`,
   11426: `Fleuve Cendre Ferryman`,
@@ -256,38 +156,20 @@ const enkaConvertWeap = {
   15512: `The First Great Magic`,
   12425: `Tidal Shadow`,
   11424: `Wolf-Fang`,
-  
-  // Version 3.8
-  
-  // Version 3.7
   15419: `Ibis Piercer`,
-  
-  // Version 3.6
   14505: `Jadefall's Splendor`,
-  
-  // Version 3.5
   12511: `Beacon of the Reed Sea`,
   12418: `Mailed Flower`,
-  
-  // Version 3.4
   11512: `Light of Foliar Incision`,
-  
-  // Version 3.3
   11422: `Toukabou Shigure`,
   14512: `Tulaytullah's Remembrance`,
-  
-  // Version 3.2
   14511: `A Thousand Floating Dreams`,
-  
-  // Version 3.1
   11511: `Key of Khaj-Nisut`,
   12415: `Makhaira Aquamarine`,
   13419: `Missive Windspear`,
   13511: `Staff of the Scarlet Sands`,
   14416: `Wandering Evenstar`,
   11418: `Xiphos' Moonlight`,
-  
-  // Version 3.0
   15418: `End of the Line`,
   12417: `Forest Regalia`,
   14417: `Fruit of Fulfillment`,
@@ -295,42 +177,24 @@ const enkaConvertWeap = {
   15417: `King's Squire`,
   13417: `Moonpiercer`,
   11417: `Sapwood Blade`,
-  
-  // Version 2.8
   11416: `Kagotsurube Isshin`,
-  
-  // Version 2.7
   15508: `Aqua Simulacra`,
   15411: `Fading Twilight`,
-  
-  // Version 2.6
   11510: `Haran Geppaku Futsu`,
-  
-  // Version 2.5
   14509: `Kagura's Verity`,
   14415: `Oathsworn Eye`,
-  
-  // Version 2.4
   13507: `Calamity Queller`,
-  
-  // Version 2.3
   11415: `Cinnabar Spindle`,
   12510: `Redhorn Stonethresher`,
-  
-  // Version 2.2
   12416: `Akuoumaru`,
   15416: `Mouun's Moon`,
   15507: `Polar Star`,
   13416: `Wavebreaker's Fin`,
-  
-  // Version 2.1
   13509: `Engulfing Lightning`,
   14506: `Everlasting Moonglow`,
   12412: `Luxurious Sea-Lord`,
   15415: `Predator`,
   13415: `"The Catch"`,
-  
-  // Version 2.0
   11414: `Amenoma Kageuchi`,
   14414: `Hakushin Ring`,
   15414: `Hamayumi`,
@@ -338,42 +202,28 @@ const enkaConvertWeap = {
   13414: `Kitain Cross Spear`,
   11509: `Mistsplitter Reforged`,
   15509: `Thundering Pulse`,
-  
-  // Version 1.6
   14413: `Dodoco Tales`,
   11503: `Freedom-Sworn`,
   15412: `Mitternachts Waltz`,
-  
-  // Version 1.5
   12503: `Song of Broken Pines`,
-  
-  // Version 1.4
   15410: `Alley Hunter`,
   15503: `Elegy for the End`,
   11410: `The Alley Flash`,
   15413: `Windblume Ode`,
   14410: `Wine and Song`,
-  
-  // Version 1.3
   12410: `Lithic Blade`,
   13406: `Lithic Spear`,
   11505: `Primordial Jade Cutter`,
   13501: `Staff of Homa`,
-  
-  // Version 1.2
   13409: `Dragonspine Spear`,
   11413: `Festering Desire`,
   14412: `Frostbearer`,
   12411: `Snow-Tombed Starsilver`,
   11504: `Summit Shaper`,
-  
-  // Version 1.1
   14504: `Memory of Dust`,
   13408: `Royal Spear`,
   12504: `The Unforged`,
   13504: `Vortex Vanquisher`,
-  
-  // Version 1.0
   15502: `Amos' Bow`,
   11501: `Aquila Favonia`,
   13303: `Black Tassel`,
