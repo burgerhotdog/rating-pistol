4eb6ecc3842ed7d98ab72e4a86ea6ada09d13ed9
Darren Wang <darrenwang2000@gmail.com>
2025-03-06 13:05:12 -0500

    lowercase

--- src/components/getIcons.js
+++ src/components/getIcons.js
@@ -1,7 +1,7 @@
 import GI from "../assets/gi/GI"
-import HSR from "../assets/HSR/HSR"
-import WW from "../assets/WW/WW"
-import ZZZ from "../assets/ZZZ/ZZZ"
+import HSR from "../assets/hsr1/HSR"
+import WW from "../assets/ww1/WW"
+import ZZZ from "../assets/zzz1/ZZZ"
 
 export default (gameType) => {
   const data = { GI, HSR, WW, ZZZ };
