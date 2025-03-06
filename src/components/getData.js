8131650873395907bff39175099b4a391281bf90
Darren Wang <darrenwang2000@gmail.com>
2025-03-06 13:02:32 -0500

    testagain

--- src/components/getData.js
+++ src/components/getData.js
@@ -1,7 +1,7 @@
 import GI from "../data/gi/GI";
-import HSR from "../data/HSR/HSR";
-import WW from "../data/WW/WW";
-import ZZZ from "../data/ZZZ/ZZZ";
+import HSR from "../data/HSR2/HSR";
+import WW from "../data/WW2/WW";
+import ZZZ from "../data/ZZZ2/ZZZ";
 
 export default (gameType) => {
   const data = { GI, HSR, WW, ZZZ };
