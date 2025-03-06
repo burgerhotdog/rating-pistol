cf6d7caf2a330061ce85efddefc43471baa2321d
Darren Wang <darrenwang2000@gmail.com>
2025-03-06 13:18:43 -0500

    1

--- src/components/getData.js
+++ src/components/getData.js
@@ -1,7 +1,7 @@
-import GI from "../data/gi/GI";
-import HSR from "../data/hsr/HSR";
-import WW from "../data/ww/WW";
-import ZZZ from "../data/zzz/ZZZ";
+import GI from "../data/gi/gi1";
+import HSR from "../data/hsr/hsr1";
+import WW from "../data/ww/ww1";
+import ZZZ from "../data/zzz/zzz1";
 
 export default (gameType) => {
   const data = { GI, HSR, WW, ZZZ };
