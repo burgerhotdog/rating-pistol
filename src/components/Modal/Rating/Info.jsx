d35843640b464260806d7c1db627c46f3ba21d59
darrenwang18 <darrenwang2000@gmail.com>
2025-05-13 18:51:35 -0400

    save

--- src/components/Modal/Rating/Info.jsx
+++ src/components/Modal/Rating/Info.jsx
@@ -1,26 +1,51 @@
-import { Paper, Box, Typography, Divider, Stack } from "@mui/material";
+import { Paper, Box, Typography, Divider, Stack, Tooltip, IconButton } from "@mui/material";
+import { HelpOutline } from "@mui/icons-material";
 import { AVATAR_DATA, LABEL_DATA } from "@data";
 
 const Info = ({ gameId, avatarId, isFullBuild, ratingData }) => {
-  const { percentile, score, q3 } = ratingData;
+  const { percentile, score, mean, sd, bounds, q3 } = ratingData;
 
   return (
-    <Paper sx={{ padding: 3, width: 350 }}>
-      <Box sx={{ mb: 2 }}>
-        <Typography variant="h6" sx={{ color: "primary.main" }} gutterBottom>
+    <Paper sx={{ padding: 3, width: 300 }}>
+      <Box>
+        <Stack direction="row" alignItems="center" spacing={1}>
+          <Typography variant="h6" sx={{ color: "primary.main" }}>
+            Simulation Results:
+          </Typography>
+          <HelpOutline fontSize="small" color="primary" />
+        </Stack>
+        <Typography variant="body2">
           Roll Value: {score.toFixed()}%
         </Typography>
-        <Typography variant="body1" gutterBottom>
+        <Typography variant="body2" sx={{ color: "text.secondary" }} >
           Percentile: {percentile.toFixed(2)}
         </Typography>
-        {!isFullBuild && (
-          <Typography variant="body1" sx={{ color: "text.disabled" }} gutterBottom>
-            Q3: {q3.toFixed()}%
+        {isFullBuild ? (
+          <>
+            <Typography variant="body2" gutterBottom sx={{ color: "text.secondary" }}>
+              Mean: {mean.toFixed()}% / SD: {sd.toFixed()}%
+            </Typography>
+            <Typography variant="body2">
+              Thresholds:
+            </Typography>
+            <Typography variant="body2" sx={{ color: "text.secondary" }}>
+              Excellent: {bounds[0].toFixed()}%
+            </Typography>
+            <Typography variant="body2" sx={{ color: "text.secondary" }}>
+              Great: {bounds[1].toFixed()}%
+            </Typography>
+            <Typography variant="body2" sx={{ color: "text.secondary" }}>
+              Good: {bounds[2].toFixed()}%
+            </Typography>
+          </>
+        ) : (
+          <Typography variant="body2" sx={{ color: "text.secondary" }}>
+            Q3 Cutoff: {q3.toFixed()}%
           </Typography>
         )}
         <Divider sx={{ my: 2 }} />
-        <Typography variant="h6" sx={{ color: "primary.main" }} gutterBottom>
-          Analysis
+        <Typography variant="h6" sx={{ color: "primary.main" }}>
+          Summary
         </Typography>
       </Box>
     </Paper>
