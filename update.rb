bd54897d4272c9f2ec3282ed6c7e288c7a222b20
darrenwang18 <darrenwang2000@gmail.com>
2025-08-19 17:50:10 -0400

    test

--- update.rb
+++ update.rb
@@ -2,104 +2,128 @@ require 'selenium-webdriver'
 require 'nokogiri'
 require 'json'
 require 'open-uri'
+# LAUMA = 10000119
 
-BASE_URL = "https://gi20.hakush.in/character/"
-ELEMENTS = ['Anemo', 'Cryo', 'Dendro', 'Electro', 'Geo', 'Hydro', 'Pyro']
-CHAR_IDS = [10000119, 10000120, 10000121]
-GAME_ID = 'gi'
+VALID_GAME_IDS = ['gi', 'hsr', 'ww', 'zzz']
+unless ARGV.length == 1 && VALID_GAME_IDS.include?(ARGV[0])
+  abort "Usage: ruby update.rb <#{VALID_GAME_IDS.join('|')}>"
+end
+
+GAME_ID = ARGV[0]
+BASE_URL = {
+  gi: "https://gi20.hakush.in/",
+  hsr: "https://hsr20.hakush.in/",
+  ww: "https://ww2.hakush.in/",
+  zzz: "https://zzz3.hakush.in/",
+}
+
+ELEMENTS = {
+  gi: ['Anemo', 'Cryo', 'Dendro', 'Electro', 'Geo', 'Hydro', 'Pyro'],
+  hsr: [],
+}
 
 options = Selenium::WebDriver::Chrome::Options.new
 options.add_argument('--headless')
 options.add_argument('--disable-gpu')
 options.add_argument('--window-size=1920,1080')
+options.add_argument('--log-level=3')
 
-driver = Selenium::WebDriver.for :chrome, options: options
-output = {}
-
-CHAR_IDS.each do |id|
-  url = "#{BASE_URL}#{id}"
-  puts "Fetching #{url}..."
-  driver.get(url)
+def load_html(url, selector)
   wait = Selenium::WebDriver::Wait.new(timeout: 10)
+  driver.get(url)
   begin
-    wait.until { driver.find_element(css: '#character-kit .grid-cols-2') }
+    wait.until { driver.find_element(css: selector) }
   rescue Selenium::WebDriver::Error::TimeoutError
     puts "Timeout Error"
-    next
+    return nil
   end
-
-  doc = Nokogiri::HTML(driver.page_source)
-  character = {
-    name: '',
-    rarity: '',
-    element: '',
-    type: '',
-    sig: 0,
-    baseStats: {
-      _HP: 0,
-      _ATK: 0,
-      _DEF: 0,
-    },
-    weights: {}
-  }
-
-  title = doc.at_css('#char-name-info')
-  character[:name] = title.at_css('div.char-name-text')&.text&.strip
-  character[:rarity] = title.css('span.char-name > div.flex.stars img.star-icon').size
-  character.delete(:sig) if character[:rarity] == 4
-
-  doc.css('#character-kit .grid-cols-2').each do |row|
-    divs = row.css('div')
-    next unless divs.size >= 2
-
-    key   = divs[0]&.text&.strip
-    value = divs[1]&.text&.strip
-
-    case key
-    when "Weapon" then character[:type] = value
-    when "Base HP" then character[:baseStats][:_HP] = value.to_i
-    when "Base ATK" then character[:baseStats][:_ATK] = value.to_i
-    when "Base DEF" then character[:baseStats][:_DEF] = value.to_i
-    else
-      if ELEMENTS.include?(value)
-        character[:element] = value
-      end
-    end
-  end
-
-  output[id] = character
+  Nokogiri::HTML(driver.page_source)
 end
 
-driver.get(BASE_URL)
-wait = Selenium::WebDriver::Wait.new(timeout: 10)
-begin
-  wait.until { driver.find_element(css: 'div.grid.grid-cols-4') }
-rescue Selenium::WebDriver::Error::TimeoutError
-  puts "Timeout Error"
-  exit
-end
+loop do
+  puts "Enter <a|w> for avatar|weapon, or <q> to quit:"
+  input = STDIN.gets.strip
 
-main_doc = Nokogiri::HTML(driver.page_source)
+  case input
+  when 'a'
+    puts "Enter avatar id:"
+    id = STDIN.gets.strip
 
-CHAR_IDS.each do |id|
-  a_tag = main_doc.at_css("a[href='/character/#{id}']")
-  if a_tag
-    img_tag = a_tag.at_css('img[alt="character icon"]')
-    if img_tag
-      img_url = img_tag['src']
-      puts "Downloading image: #{img_url}"
-      filename = File.join('src', 'assets', 'dynamic', 'avatar', "#{GAME_ID}_avatar", "#{id}.webp")
-      URI.open(img_url) do |image|
-        File.open(filename, 'wb') { |file| file.write(image.read) }
-      end
-      puts "Saved as #{filename}"
-    else
-      puts "Error: No icon found for #{id}"
+    driver = Selenium::WebDriver.for :chrome, options: options
+    url_tag = GAME_ID == 'hsr' ? 'char/' : 'character/'
+    url = "#{BASE_URL[GAME_ID.to_sym]}#{url_tag}"
+
+    # icon
+    doc_icon = load_html(url, 'div.grid.grid-cols-4')
+    next unless doc_icon
+
+    img_url = doc_icon.at_css("a[href='/#{url_tag}#{id}'] > img.avatar-icon-front")&.[]('src')
+    puts "Downloading image: #{img_url}"
+    filename = File.join('src', 'assets', 'dynamic', 'avatar', "#{GAME_ID}_avatar", "#{id}.webp")
+    URI.open(img_url) do |image|
+      File.open(filename, 'wb') { |file| file.write(image.read) }
     end
+    puts "Saved as #{filename}"
+
+    # data
+    doc_data = load_html("#{url}#{id}", '#character-kit')
+    next unless doc_data
+
+    data = {
+      name: '',
+      rarity: '',
+      element: '',
+      type: '',
+      sig: 0,
+      baseStats: {
+        _HP: 0,
+        _ATK: 0,
+        _DEF: 0,
+      },
+      weights: {},
+    }
+
+    title = doc.at_css('#char-name-info')
+    data[:name] = title.at_css('div.char-name-text')&.text&.strip
+    data[:rarity] = title.css('span.char-name > div.stars img.star-icon').size
+    data.delete(:sig) if data[:rarity] == 4
+    if GAME_ID == 'ww' || GAME_ID == 'zzz'
+      data[:element] = title.css('span.char-name > div.base-type-text')&.text&.strip
+    end
+
+    doc.css('#character-kit > .grid:first-child .grid-cols-2').each do |row|
+      divs = row.css('div')
+      next unless divs.size == 2
+
+      key = divs[0]&.text&.strip
+      value = divs[1]&.text&.strip
+
+      case key
+      when "Base HP" then data[:baseStats][:_HP] = value.to_i
+      when "Base ATK" then data[:baseStats][:_ATK] = value.to_i
+      when "Base DEF" then data[:baseStats][:_DEF] = value.to_i
+      when "Weapon" then data[:type] = value if GAME_ID != 'hsr'
+      when "Path" then data[:type] = value.split[1] if GAME_ID == 'hsr'
+      else
+        if GAME_ID == 'gi' || GAME_ID == 'hsr'
+          if ELEMENTS[GAME_ID.to_sym].include?(value)
+            data[:element] = value
+          end
+        end
+      end
+    end
+
+    puts JSON.pretty_generate(data)
+
+    driver.quit
+    puts
+  when 'w'
+    break
+  when 'q'
+    break
   else
-    puts "Error: No link found for #{id}"
+    puts "Invalid input, please try again."
   end
 end
 
-puts JSON.pretty_generate(output)
-driver.quit
+exit 1
