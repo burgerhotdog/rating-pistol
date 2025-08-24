e2ca2e7f4cf0ef1e65331fa36ba4561d370e2025
darrenwang18 <darrenwang2000@gmail.com>
2025-08-24 01:58:34 -0400

    yes

--- src/components/Modal/Rating/Info.jsx
+++ src/components/Modal/Rating/Info.jsx
@@ -2,12 +2,50 @@ import { Paper, Box, Typography, Divider, Stack } from '@mui/material';
 import { AVATAR_ASSETS, RATING_ASSETS } from '@assets';
 import { AVATAR_DATA, INFO_DATA, STAT_DATA } from '@data';
 
+const how_many_days = {
+  gi: (pieces) => {
+    const types_of_pieces = 5;
+    const pieces_per_run = 1.25;
+    const runs = (pieces * types_of_pieces) / pieces_per_run;
+    const resin_per_run = 20;
+    const resin_per_day = 200;
+    const days = (runs * resin_per_run) / resin_per_day;
+    return [runs, days];
+  },
+  hsr: (pieces) => {
+    const types_of_pieces = 3;
+    const pieces_per_run = 2.25;
+    const runs = (pieces * types_of_pieces) / pieces_per_run;
+    const resin_per_run = 40;
+    const resin_per_day = 240;
+    const days = (runs * resin_per_run) / resin_per_day;
+    return [runs, days];
+  },
+  ww: (pieces) => {
+    const types_of_pieces = 2;
+    const pieces_per_run = 5;
+    const runs = (pieces * types_of_pieces) / pieces_per_run;
+    const resin_per_run = 60;
+    const resin_per_day = 240;
+    const days = (runs * resin_per_run) / resin_per_day;
+    return [runs, days];
+  },
+  zzz: (pieces) => {
+    const types_of_pieces = 6;
+    const pieces_per_run = 2.5;
+    const runs = (pieces * types_of_pieces) / pieces_per_run;
+    const resin_per_run = 60;
+    const resin_per_day = 320;
+    const days = (runs * resin_per_run) / resin_per_day;
+    return [runs, days];
+  },
+};
+
 const Info = ({ gameId, avatarId, index, ratingData, stat }) => {
   const { score, q3, percentile } = ratingData;
 
   const times_for_specific_piece = (10000 / (10000 - (percentile * 100)));
   const times_for_piece = (times_for_specific_piece / STAT_DATA[gameId][stat].mainChance[index]);
-  const domain_runs = times_for_piece * INFO_DATA[gameId].MAIN_LEN * 2;
 
   return (
     <Paper sx={{ p: 3, width: 300 }}>
@@ -24,7 +62,7 @@ const Info = ({ gameId, avatarId, index, ratingData, stat }) => {
         Q3: {q3.toFixed()}%
       </Typography>
       <Typography variant="body2" sx={{ color: 'text.secondary' }}>
-        It will take approximately {domain_runs.toFixed()} domain runs to get a better {INFO_DATA[gameId].EQUIP_NAMES[index]}. ({(domain_runs / 6).toFixed()} days)
+        It will take approximately {how_many_days[gameId](times_for_piece)[0].toFixed()} domain runs to get a better {INFO_DATA[gameId].EQUIP_NAMES[index]}. ({how_many_days[gameId](times_for_piece)[1].toFixed()} days)
       </Typography>
     </Paper>
   );
