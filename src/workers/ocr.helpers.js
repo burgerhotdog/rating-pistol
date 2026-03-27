68d730f819b289e64440b32813edc9c128edbd6d
Darren Wang <darrenwang2000@gmail.com>
2026-03-27 11:46:32 -0400

    save

--- src/workers/ocr.helpers.js
+++ src/workers/ocr.helpers.js
@@ -1,16 +1,41 @@
 import { distance } from 'fastest-levenshtein';
+import { WEAPON_LOOKUP, GENERAL_LOOKUP } from '@/lookups';
 
-export async function cropBlob(region, bitmap) {
+export const WEAPON_DICT = Object.fromEntries(
+  Object.entries(WEAPON_LOOKUP['wuthering-waves']).map(([id, { NAME }]) => ([NAME, id]))
+);
+
+const { MAIN_STAT_TYPES, SUB_STAT_TYPES } = GENERAL_LOOKUP['wuthering-waves'];
+export const MAIN_STAT_DICT_PER_COST = Object.fromEntries(MAIN_STAT_TYPES.map((map, index) => {
+  const dict = {};
+  for (const [id, { NAME }] of Object.entries(map)) {
+    dict[NAME] = id;
+  }
+  switch (index) {
+    case 0:
+      return [1, dict];
+    case 1:
+      return [3, dict];
+    case 2:
+      return [4, dict];
+  }
+}));
+
+export const SUBSTAT_DICT = Object.fromEntries(
+  Object.entries(SUB_STAT_TYPES).map(([id, { NAME }]) => ([NAME, id]))
+);
+
+export async function cropBlob(region, imageBitmap) {
   const cropCanvas = new OffscreenCanvas(region.w, region.h);
   const cropCtx = cropCanvas.getContext('2d');
-  cropCtx.drawImage(bitmap, region.x, region.y, region.w, region.h, 0, 0, region.w, region.h);
+  cropCtx.drawImage(imageBitmap, region.x, region.y, region.w, region.h, 0, 0, region.w, region.h);
   return cropCanvas.convertToBlob();
 };
 
-export async function cropPixels(region, bitmap) {
+export async function cropPixels(region, imageBitmap) {
   const cropCanvas = new OffscreenCanvas(region.w, region.h);
   const cropCtx = cropCanvas.getContext('2d');
-  cropCtx.drawImage(bitmap, region.x, region.y, region.w, region.h, 0, 0, region.w, region.h);
+  cropCtx.drawImage(imageBitmap, region.x, region.y, region.w, region.h, 0, 0, region.w, region.h);
   return cropCtx.getImageData(0, 0, region.w, region.h).data;
 };
 
--- src/workers/ocr.worker.js
+++ src/workers/ocr.worker.js
@@ -1,13 +1,9 @@
 import { createWorker } from 'tesseract.js';
-import { WEAPON_LOOKUP, GENERAL_LOOKUP } from '@/lookups';
-import { cropBlob, cropPixels, snapToOption } from './ocr.helpers';
+import { GENERAL_LOOKUP } from '@/lookups';
+import { cropBlob, cropPixels, snapToOption, WEAPON_DICT, SUBSTAT_DICT, MAIN_STAT_DICT_PER_COST } from './ocr.helpers';
 import { COST_TEMPLATES } from '@/assets/ocr/cost/templates';
 
-const weaponsList = Object.entries(WEAPON_LOOKUP['wuthering-waves']).map(([id, { NAME }]) => ([NAME, id]));
-const weaponsDict = Object.fromEntries(weaponsList);
-
-const { MAIN_STAT_TYPES, SUB_STAT_TYPES } = GENERAL_LOOKUP['wuthering-waves'];
-const subStatDict = Object.entries(SUB_STAT_TYPES).map(([id, { NAME }]) => ([NAME, id]));
+const { MAIN_STAT_TYPES } = GENERAL_LOOKUP['wuthering-waves'];
 
 let worker = null;
 
@@ -54,12 +50,18 @@ self.onmessage = async (e) => {
 
   try {
     const ocrWorker = await initWorker();
+
+    async function regionToText(region, dict) {
+      const blob = await cropBlob(region, imageBitmap);
+      const { data: { text } } = await ocrWorker.recognize(blob);
+      const validatedText = await snapToOption(text.trim(), Object.keys(dict));
+      return validatedText;
+    }
+
     const results = {};
 
     const weaponNameRegion = { x: 1600, y: 450, w: 250, h: 30};
-    const blob = await cropBlob(weaponNameRegion, imageBitmap);
-    const { data: { text } } = await ocrWorker.recognize(blob);
-    results.weaponId = weaponsDict[snapToOption(text.trim(), Object.keys(weaponsDict))];
+    results.weaponId = WEAPON_DICT[regionToText(weaponNameRegion, WEAPON_DICT)];
 
     const slot1 = { x: 323, y: 664, w: 47, h: 47};
     const slot2 = { x: 696, y: 664, w: 47, h: 47};
@@ -73,54 +75,43 @@ self.onmessage = async (e) => {
     const cost4 = findBestMatch(await cropPixels(slot4, imageBitmap), COST_TEMPLATES);
     const cost5 = findBestMatch(await cropPixels(slot5, imageBitmap), COST_TEMPLATES);
 
-    results.list = [cost1, cost2, cost3, cost4, cost5];
-
     // slot 1
     const equipObj1 = {};
     const main1 = { x: 219, y: 727, w: 159, h: 20};
-    const main1Blob = await cropBlob(main1, imageBitmap);
-    const { data: { text: main1Text } } = await ocrWorker.recognize(main1Blob);
-    console.log(snapToOption(main1Text.trim(), Object.entries(MAIN_STAT_TYPES[costConvert[cost1]]).map(([id, { NAME }]) => ([NAME, id]))));
-    equipObj1.mainStatId = Object.fromEntries(Object.entries(MAIN_STAT_TYPES[costConvert[cost1]]).map(([id, { NAME }]) => ([NAME, id])))[snapToOption(main1Text.trim(), Object.entries(MAIN_STAT_TYPES[costConvert[cost1]]).map(([id, { NAME }]) => ([NAME, id])))];
+    equipObj1.mainStatId = MAIN_STAT_DICT_PER_COST[cost1][regionToText(main1, MAIN_STAT_DICT_PER_COST[cost1])];
     equipObj1.mainStatValue = MAIN_STAT_TYPES[costConvert[cost1]][equipObj1.mainStatId].VALUE;
-
-    console.log(equipObj1);
     
     const m1line1 = {};
     const m1sub1 = { x: 63, y: 884, w: 394, h: 22};
-    const m1sub1Blob = await cropBlob(m1sub1, imageBitmap);
-    const { data: { text: m1sub1Text } } = await ocrWorker.recognize(m1sub1Blob);
-    m1line1.subStatId = subStatDict[snapToOption(m1sub1Text.trim(), Object.keys(Object.fromEntries(subStatDict)))];
+    m1line1.subStatId = SUBSTAT_DICT[regionToText(m1sub1, SUBSTAT_DICT)];
     const m1val1 = { x: 300, y: 884, w: 73, h: 22};
-    const m1val1Blob = await cropBlob(m1val1, imageBitmap);
-    const { data: { text: m1val1Text } } = await ocrWorker.recognize(m1val1Blob);
-    m1line1.subStatValue = Number(m1val1Text);
 
     const m1line2 = {};
     const m1sub2 = { x: 63, y: 918, w: 394, h: 22};
+    m1line2.subStatId = SUBSTAT_DICT[regionToText(m1sub2, SUBSTAT_DICT)];
     const m1val2 = { x: 300, y: 884, w: 73, h: 22};
 
     const m1line3 = {};
     const m1sub3 = { x: 63, y: 952, w: 394, h: 22};
+    m1line3.subStatId = SUBSTAT_DICT[regionToText(m1sub3, SUBSTAT_DICT)];
     const m1val3 = { x: 300, y: 884, w: 73, h: 22};
 
     const m1line4 = {};
     const m1sub4 = { x: 63, y: 986, w: 394, h: 22};
+    m1line4.subStatId = SUBSTAT_DICT[regionToText(m1sub4, SUBSTAT_DICT)];
     const m1val4 = { x: 300, y: 884, w: 73, h: 22};
 
     const m1line5 = {};
     const m1sub5 = { x: 63, y: 1020, w: 394, h: 22};
+    m1line5.subStatId = SUBSTAT_DICT[regionToText(m1sub5, SUBSTAT_DICT)];
     const m1val5 = { x: 300, y: 884, w: 73, h: 22};
 
     equipObj1.subStatList = [m1line1, m1line2, m1line3, m1line4, m1line5]
   
+    // slot 2
     const equipObj2 = {};
     const main2 = { x: 594, y: 727, w: 159, h: 20};
-    const main2Blob = await cropBlob(main2, imageBitmap);
-    const { data: { text: main2Text } } = await ocrWorker.recognize(main2Blob);
-    equipObj2.mainStatId = Object.fromEntries(Object.entries(MAIN_STAT_TYPES[costConvert[cost2]]).map(([id, { NAME }]) => ([NAME, id])))[snapToOption(main2Text.trim(), Object.entries(MAIN_STAT_TYPES[costConvert[cost2]]).map(([id, { NAME }]) => ([NAME, id])))];
-    equipObj2.mainStatValue = MAIN_STAT_TYPES[costConvert[cost2]][equipObj2.mainStatId].VALUE;
-
+    equipObj2.mainStatId = MAIN_STAT_DICT_PER_COST[cost2][regionToText(main2, MAIN_STAT_DICT_PER_COST[cost2])];
     const m2sub1 = { x: 438, y: 884, w: 394, h: 22};
     const m2sub2 = { x: 438, y: 918, w: 394, h: 22};
     const m2sub3 = { x: 438, y: 952, w: 394, h: 22};
@@ -132,13 +123,10 @@ self.onmessage = async (e) => {
     const m2val4 = { x: 678, y: 884, w: 73, h: 22};
     const m2val5 = { x: 678, y: 884, w: 73, h: 22};
   
+    // slot 3
     const equipObj3 = {};
     const main3 = { x: 968, y: 727, w: 159, h: 20};
-    const main3Blob = await cropBlob(main3, imageBitmap);
-    const { data: { text: main3Text } } = await ocrWorker.recognize(main3Blob);
-    equipObj3.mainStatId = Object.fromEntries(Object.entries(MAIN_STAT_TYPES[costConvert[cost3]]).map(([id, { NAME }]) => ([NAME, id])))[snapToOption(main3Text.trim(), Object.entries(MAIN_STAT_TYPES[costConvert[cost3]]).map(([id, { NAME }]) => ([NAME, id])))];
-    equipObj3.mainStatValue = MAIN_STAT_TYPES[costConvert[cost3]][equipObj3.mainStatId].VALUE;
-
+    equipObj3.mainStatId = MAIN_STAT_DICT_PER_COST[cost3][regionToText(main3, MAIN_STAT_DICT_PER_COST[cost3])];
     const m3sub1 = { x: 812, y: 884, w: 394, h: 22};
     const m3sub2 = { x: 812, y: 918, w: 394, h: 22};
     const m3sub3 = { x: 812, y: 952, w: 394, h: 22};
@@ -150,13 +138,10 @@ self.onmessage = async (e) => {
     const m3val4 = { x: 1052, y: 884, w: 73, h: 22};
     const m3val5 = { x: 1052, y: 884, w: 73, h: 22};
   
+    // slot 4
     const equipObj4 = {};
     const main4 = { x: 1342, y: 727, w: 159, h: 20};
-    const main4Blob = await cropBlob(main4, imageBitmap);
-    const { data: { text: main4Text } } = await ocrWorker.recognize(main4Blob);
-    equipObj4.mainStatId = Object.fromEntries(Object.entries(MAIN_STAT_TYPES[costConvert[cost4]]).map(([id, { NAME }]) => ([NAME, id])))[snapToOption(main4Text.trim(), Object.entries(MAIN_STAT_TYPES[costConvert[cost4]]).map(([id, { NAME }]) => ([NAME, id])))];
-    equipObj4.mainStatValue = MAIN_STAT_TYPES[costConvert[cost4]][equipObj4.mainStatId].VALUE;
-
+    equipObj4.mainStatId = MAIN_STAT_DICT_PER_COST[cost4][regionToText(main4, MAIN_STAT_DICT_PER_COST[cost4])];
     const m4sub1 = { x: 1185, y: 884, w: 394, h: 22};
     const m4sub2 = { x: 1185, y: 918, w: 394, h: 22};
     const m4sub3 = { x: 1185, y: 952, w: 394, h: 22};
@@ -168,13 +153,10 @@ self.onmessage = async (e) => {
     const m4val4 = { x: 1426, y: 884, w: 73, h: 22};
     const m4val5 = { x: 1426, y: 884, w: 73, h: 22};
   
+    // slot 5
     const equipObj5 = {};
     const main5 = { x: 1715, y: 727, w: 159, h: 20};
-    const main5Blob = await cropBlob(main5, imageBitmap);
-    const { data: { text: main5Text } } = await ocrWorker.recognize(main5Blob);
-    equipObj5.mainStatId = Object.fromEntries(Object.entries(MAIN_STAT_TYPES[costConvert[cost5]]).map(([id, { NAME }]) => ([NAME, id])))[snapToOption(main5Text.trim(), Object.entries(MAIN_STAT_TYPES[costConvert[cost5]]).map(([id, { NAME }]) => ([NAME, id])))];
-    equipObj5.mainStatValue = MAIN_STAT_TYPES[costConvert[cost5]][equipObj5.mainStatId].VALUE;
-
+    equipObj5.mainStatId = MAIN_STAT_DICT_PER_COST[cost5][regionToText(main5, MAIN_STAT_DICT_PER_COST[cost5])];
     const m5sub1 = { x: 1560, y: 884, w: 394, h: 22};
     const m5sub2 = { x: 1560, y: 918, w: 394, h: 22};
     const m5sub3 = { x: 1560, y: 952, w: 394, h: 22};
