e6e372164ba36e0a31cfadc51a7448a784bae6f4
Darren <59891325+burgerhotdog@users.noreply.github.com>
2026-05-02 20:17:56 -0400

    save

--- src/workers/simulation/helpers/simulateRotation.js
+++ src/workers/simulation/helpers/simulateRotation.js
@@ -1,7 +1,7 @@
-import { CHARACTERS, MVS, RATING } from '@/data';
+import { CHARACTERS, RATING } from '@/data';
 import { compileStatMap, mergeStatMaps, getSkill } from '@/utils';
 
-function simulateAction(gameId, actionKey, statMap) {
+function simulateAction(gameId, actionKey, statMap, activeEffectMap) {
   const [characterId, skillId, actionId] = actionKey.split('-');
   const { element } = CHARACTERS[gameId][characterId];
   const { computeBase, computeBonuses, computeReductions } = RATING[gameId];
@@ -17,7 +17,13 @@ function simulateAction(gameId, actionKey, statMap) {
   if (considered === "BUFF") return {};
   if (!multipliers) return {};
   const dmgTypes = [considered, special].filter(Boolean);
-  const adjustedStatMap = modifiers ? mergeStatMaps(statMap, modifiers) : statMap;
+  const effectStatMap = Object.entries(activeEffectMap).map(([effectKey, stacks]) => {
+    const [ownerId, ei] = effectKey.split('-');
+    const { statMap = {} } = CHARACTERS[gameId][ownerId].effects[ei];
+    return Object.fromEntries(Object.entries(statMap).map(([statId, statValue]) => ([statId, statValue * stacks])));
+  }).reduce((acc, statMap) => mergeStatMaps(acc, statMap), {});
+  const statMapWithEffects = mergeStatMaps(statMap, effectStatMap);
+  const adjustedStatMap = modifiers ? mergeStatMaps(statMapWithEffects, modifiers) : statMapWithEffects;
   
   const baseDmg = computeBase(adjustedStatMap, attr, multipliers);
   const bonuses = computeBonuses(adjustedStatMap, element, dmgTypes);
@@ -30,17 +36,14 @@ function parseTeammateEffects(gameId, memberId, index, team) {
   const teammateOrder = [
     ...team.slice(index),
     ...team.slice(0, index),
-  ].reverse();
+  ].slice(1).reverse();
 
   const fullActionOrder = teammateOrder.flatMap(member => member.rotation);
 
-  let effectStatMap = {};
+  const activeEffects = {};
   for (const actionKey of fullActionOrder) {
     const [ownerId, skillId, actionId] = actionKey.split('-');
     if (ownerId === memberId) continue;
-
-    const ownerEffects = CHARACTERS[gameId][ownerId].effects;
-    if (!ownerEffects) continue;
 
     const validTargets = [
       'team',
@@ -50,14 +53,19 @@ function parseTeammateEffects(gameId, memberId, index, team) {
       ...((team.findIndex(m => m.memberId === ownerId) === 0) && (index === team.length - 1) ? ['next'] : []),
     ];
 
-    for (const effect of ownerEffects) {
-      if (effect.trigger !== `${skillId}-${actionId}`) continue;
-      if (!validTargets.includes(effect.target)) continue;
+    const { effects = [] } = CHARACTERS[gameId][ownerId];
+    for (const [ei, { trigger, target, maxStacks = 1 }] of effects.entries()) {
+      if (trigger !== `${skillId}-${actionId}`) continue; // effect wasn't triggered by this action
+      if (!validTargets.includes(target)) continue; // effect doesn't apply to this target
 
-      effectStatMap = mergeStatMaps(effectStatMap, effect.statMap);
+      const effectKey = `${ownerId}-${ei}`;
+      const activeStacks = activeEffects[effectKey] ?? 0;
+      if (activeStacks < maxStacks) {
+        activeEffects[effectKey] = activeStacks + 1;
+      }
     }
   }
-  return effectStatMap;
+  return activeEffects;
 }
 
 export function normalizeTeam(team, teamFinalStats = {}) {
@@ -83,24 +91,25 @@ export function simulateRotation(gameId, team) {
     if (!memberId) continue;
 
     const memberEffects = CHARACTERS[gameId][memberId].effects ?? [];
-    const effectStatMap = parseTeammateEffects(gameId, memberId, index, team);
+    const activeEffectMap = parseTeammateEffects(gameId, memberId, index, team);
     const statMap = compileStatMap(gameId, memberId, build, team, 'combat');
 
-    const mergedMap = mergeStatMaps(statMap, effectStatMap);
     for (const actionKey of rotation) {
       const [ownerId, skillId, actionId] = actionKey.split('-');
       if (ownerId !== memberId) continue;
 
-      for (const effect of memberEffects) {
-        if (effect.trigger !== `${skillId}-${actionId}`) continue;
-        if (!['self', 'team'].includes(effect.target)) continue;
+      for (const [ei, { trigger, target, maxStacks = 1 }] of memberEffects.entries()) {
+        if (trigger !== `${skillId}-${actionId}`) continue;
+        if (!['self', 'team'].includes(target)) continue;
 
-        for (const [effStatId, effStatValue] of Object.entries(effect.statMap)) {
-          mergedMap[effStatId] = (mergedMap[effStatId] ?? 0) + effStatValue;
+        const effectKey = `${memberId}-${ei}`;
+        const activeStacks = activeEffectMap[effectKey] ?? 0;
+        if (activeStacks < maxStacks) {
+          activeEffectMap[effectKey] = activeStacks + 1;
         }
       }
   
-      const { damage = 0, healing = 0 } = simulateAction(gameId, actionKey, mergedMap);
+      const { damage = 0, healing = 0 } = simulateAction(gameId, actionKey, statMap, activeEffectMap);
       actionMap[actionKey] = {
         damage: (actionMap[actionKey]?.damage ?? 0) + damage,
         healing: (actionMap[actionKey]?.healing ?? 0) + healing,
