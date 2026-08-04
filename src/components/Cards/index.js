608424fed6b025a437c09824ced552066e9dbf1e
Darren <59891325+burgerhotdog@users.noreply.github.com>
2026-08-04 13:10:17 -0400

    save

--- src/components/Cards/index.js
+++ src/components/Cards/index.js
@@ -2,7 +2,5 @@ export * from './ProgressChart';
 export * from './RatingGrade';
 export * from './Timeline';
 export * from './DamageDistribution';
-export * from './MainstatDistribution';
-export * from './SubstatDistribution';
 export * from './MainstatDist';
 export * from './SubstatDist';
