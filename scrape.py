d6591a5f1a4077d00fdf0692130f2e4cd5a89396
Darren Wang <darrenwang2000@gmail.com>
2026-09-11 20:10:34 -0400

    save

--- scrape.py
+++ scrape.py
@@ -54,14 +54,15 @@ def main():
     print("Update complete")
 
 def temp():
-    with open("src/data/zenless-zone-zero/character.json", "r", encoding="utf-8") as f:
+    with open("src/data/genshin-impact/character.json", "r", encoding="utf-8") as f:
         data = json.load(f)
         for id in data.keys():
             print(id)
-            data = requests.get(f"https://static.nanoka.cc/zzz/3.2/en/character/{id}.json").json()
-            image = requests.get(f"https://static.nanoka.cc/assets/zzz/{data['icon']}.webp").content
-            with open(f"public/zenless-zone-zero/characterFull/{id}.webp", "wb") as f:
+            data = requests.get(f"https://static.nanoka.cc/gi/7.0.54/en/character/{id}.json").json()
+            urlpart = data["icon"].replace("AvatarIcon", "Gacha_AvatarImg", 1)
+            image = requests.get(f"https://static.nanoka.cc/assets/gi/{urlpart}.webp").content
+            with open(f"public/genshin-impact/characterFull/{id}.webp", "wb") as f:
                 f.write(image)
 
 if __name__ == "__main__":
-    main()
+    temp()
